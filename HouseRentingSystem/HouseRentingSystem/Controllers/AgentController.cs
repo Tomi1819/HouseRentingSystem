17df61f5042802a151db45c6085b215ba3bd07dd
Tomi1819 <toshevtomi@gmail.com>
2024-03-08 00:30:41 +0200

    Controller edited

--- HouseRentingSystem/HouseRentingSystem/Controllers/AgentController.cs
+++ HouseRentingSystem/HouseRentingSystem/Controllers/AgentController.cs
@@ -27,7 +27,7 @@
         [NotAnAgent]
         public async Task<IActionResult> Become(BecomeAgentFormModel model)
         {
-            if (await agentService.UserWithPhoneNumberExistsAsync(User.Id()))
+            if (await agentService.UserWithPhoneNumberExistsAsync(model.PhoneNumber))
             {
                 ModelState.AddModelError(nameof(model.PhoneNumber), PhoneExists);
             }
