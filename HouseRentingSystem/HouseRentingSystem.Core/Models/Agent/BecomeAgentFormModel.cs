077e002697257b0983c03e40d29a9e176dc19998
Tomi1819 <toshevtomi@gmail.com>
2024-03-07 22:33:19 +0200

    BecomeAgentFormModel created

--- HouseRentingSystem/HouseRentingSystem.Core/Models/Agent/BecomeAgentFormModel.cs
+++ HouseRentingSystem/HouseRentingSystem.Core/Models/Agent/BecomeAgentFormModel.cs
@@ -1,6 +1,16 @@
 ﻿namespace HouseRentingSystem.Core.Models.Agent
 {
+    using System.ComponentModel.DataAnnotations;
+    using static Constants.MessageConstants;
+    using static HouseRentingSystem.Infrastructure.DataConstants.ValidationConstants;
     public class BecomeAgentFormModel
     {
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(AgentPhoneNumberMaxLength,
+            MinimumLength = AgentPhoneNumberMinLength,
+            ErrorMessage = LengthMessage)]
+        [Display(Name = "Phone number")]
+        [Phone]
+        public string PhoneNumber { get; set; } = null!;
     }
 }
