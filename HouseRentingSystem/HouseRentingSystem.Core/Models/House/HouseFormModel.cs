7553ecec8ae4770450bd287da276fb84ec21cd3e
Tomi1819 <toshevtomi@gmail.com>
2024-03-09 23:21:53 +0200

    View models added

--- HouseRentingSystem/HouseRentingSystem.Core/Models/House/HouseFormModel.cs
+++ HouseRentingSystem/HouseRentingSystem.Core/Models/House/HouseFormModel.cs
@@ -1,6 +1,49 @@
 ﻿namespace HouseRentingSystem.Core.Models.House
 {
+    using System.ComponentModel.DataAnnotations;
+    using static HouseRentingSystem.Core.Constants.MessageConstants;
+    using static HouseRentingSystem.Infrastructure.DataConstants.ValidationConstants;
+
     public class HouseFormModel
     {
+        public HouseFormModel()
+        {
+            Categories = new List<HouseCategoryServiceModel>();
+        }
+
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(HouseTitleMaxLength,
+            MinimumLength = HouseTitleMinLength,
+            ErrorMessage = LengthMessage)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(HouseAddressMaxLength,
+            MinimumLength = HouseAddressMinLength,
+            ErrorMessage = LengthMessage)]
+        public string Address { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(HouseDescriptionMaxLength,
+            MinimumLength = HouseDescriptionMinLength,
+            ErrorMessage = LengthMessage)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequiredMessage)]
+        [Display(Name = "Image URL")]
+        public string ImageUrl { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequiredMessage)]
+        [Range(typeof(decimal),
+            MinPricePerMonth,
+            MaxPricePerMonth,
+            ConvertValueInInvariantCulture = true,
+            ErrorMessage = PriceErrorMessage)]
+        [Display(Name = "Price Per Month")]
+        public decimal PricePerMonth { get; set; }
+
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+        public IEnumerable<HouseCategoryServiceModel> Categories { get; set; }
     }
 }
