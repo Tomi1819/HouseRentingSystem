ba648cc4bd90ac3a3297baf9526fe091932f417b
Tomi1819 <toshevtomi@gmail.com>
2024-03-09 23:46:25 +0200

    MustBeAgent attribute

---
+++ HouseRentingSystem/HouseRentingSystem/Attributes/MustBeAgent.cs
@@ -0,0 +1,29 @@
+﻿namespace HouseRentingSystem.Attributes
+{
+    using HouseRentingSystem.Controllers;
+    using HouseRentingSystem.Core.Contracts;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+    using System.Security.Claims;
+
+    public class MustBeAgent : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+
+            IAgentService? agentService = context.HttpContext.RequestServices.GetService<IAgentService>();
+
+            if (agentService is null)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+
+            if (agentService is not null
+                && agentService.ExistsByIdAsync(context.HttpContext.User.Id()).Result is false)
+            {
+                context.Result = new RedirectToActionResult(nameof(AgentController.Become), "Agent", null);
+            }
+        }
+    }
+}
